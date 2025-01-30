b1896b202caf8e68904a0de6a5e91de44ea5db99
Piotr Jastrzebski <piotr.jastrzebski@systell.eu>
2025-01-30 13:15:49 +0100

    memberdelete

---
+++ Models/MemberUpdate_deleteOne.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace _3CX_API_20.Models
+{
+    public class MemberUpdate_deleteOne
+    {
+        [JsonPropertyName("Number")]
+        public string Number { get; set; }
+        [JsonPropertyName("Id")]
+        public int Id { get; set; }
+    }
+    public class GroupMembersUpdate_deleteOne
+    {
+        [JsonPropertyName("value")]
+        public List<MemberUpdate_deleteOne> Value { get; set; } = new List<MemberUpdate_deleteOne>();
+        // public List<MemberUpdate_deleteOne> Members { get; set; } = new List<MemberUpdate_deleteOne>();
+    }
+
+    public class MemberUpdate_deleteOne_add
+    {
+        [JsonPropertyName("Members")]
+        public List<MemberUpdate_deleteOne> Members { get; set; } = new List<MemberUpdate_deleteOne>();
+
+    }
+}
